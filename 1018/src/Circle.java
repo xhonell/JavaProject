3c03594d061277227f94f4c63b9ac88a33d0760f
xhonell <xhonellwong@gmail.com>
2024-10-18 16:43:43 +0800

    v:20241018 18:00

---
+++ 1018/src/Circle.java
@@ -0,0 +1,28 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double girth() {
+        return Math.PI * 2 * radius;
+    }
+}
---
+++ 1018/src/Square.java
@@ -0,0 +1,5 @@
+public class Square extends Rect {
+    public Square(double x) {
+        super(x, x);
+    }
+}
